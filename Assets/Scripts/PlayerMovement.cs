136a26489614988c8e0d027438000eb7c53dd21e
jaehyeonB <didska5518@g.shingu.ac.kr>
2025-04-17 16:38:25 +0900

    [Feat] 유니티 스테이지 추가 밑 미완성된 플레이어 넉백 기능 추가

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-
+    [Header("플레이어 이동")]
     public float moveSpeed = 2.0f;
     public float jumpForce = 5.0f;
     public Transform groundCheck;
     public LayerMask groundLayer;
+    public float PlayerDirection = 0;
+    
+    [Header("플레이어 넉백")]
+    public float knockbackForce = 7.0f;             //넉백 힘
+    public float knockbackDuration = -1.5f;         //넉백 지속 시간
+    private bool isKnockedBack = false;             //넉백이 되었는가
+    private float KnockBackTimer = 0f;              //넉백 타이머
+
+    [Header("플레이어 HP")]
+    public int PlayerHealth = 3;
 
     private Rigidbody2D rb;
     public bool isGrounded;
@@ -21,6 +32,22 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
+
+        /*float direction = Input.GetAxis("Horizontal");
+
+        if ( direction > 0)
+        {
+            transform.localScale = new Vector3(1,1,1);
+
+        }
+        else if ( direction < 0)
+        {
+            transform.localScale = new Vector3 (-1,1,1);
+        }
+
+        PlayerDirection = direction;*/
+
+        #region 플레이어 이동
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
@@ -29,6 +56,19 @@ public class PlayerMovement : MonoBehaviour
         if(isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        //넉백 플레이어 강제 이동 + 플레이어 스턴
+        #endregion
+
+
+        if(isKnockedBack)
+        {
+            KnockBackTimer -= Time.deltaTime;
+
+            if(KnockBackTimer <= 0)
+            {
+                isKnockedBack = false;
+            }
         }
     }
     void Start()
@@ -47,6 +87,36 @@ public class PlayerMovement : MonoBehaviour
         {
             collision.GetComponent<LevelObject>().MovetoNextLevel();
         }
-    }
 
+        if ( collision.CompareTag("Enemy"))         
+        {
+            PlayerHealth --;
+            TakeDamage(collision.transform.position - transform.position);
+        }
+    }
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ( collision.CompareTag("Enemy"))         
+        {
+            PlayerHealth --;
+            TakeDamage(collision.transform.position - transform.position);
+        }
+    }*/
+    #region 너 못해
+    public void TakeDamage(Vector2 playerDirection)
+    {
+        if (!isKnockedBack)
+        {
+            // 넉백 효과 적용
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.zero; // 현재 속도를 초기화
+            rb.AddForce(-playerDirection.normalized * knockbackForce, ForceMode2D.Impulse); // 넉백 방향으로 힘을 가함
+
+ 
+            // 넉백 상태 설정 및 타이머 초기화
+            isKnockedBack = true;
+            KnockBackTimer = knockbackDuration;
+        }
+    }
+    #endregion
 }
